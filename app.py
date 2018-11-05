61b74d49beb3c74652d4a706868f7a57fb20fa6e
Janal Bhandari <janalbhandari@Janals-MacBook-Pro.local>
2018-11-05 15:35:09 -0800

    make it better

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, request
+from iexfinance import Stock
+import iexfinance.utils.exceptions as excep
+import time
+
+now = time.strftime("%c")
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST']) #allow both GET and POST requests
+def index():
+    if request.method == 'POST':  #this block is only entered when the form is submitted
+        stock = request.form['symbol']
+        symbol = Stock(stock)
+
+        name = symbol.get_company_name() + "\n\n"
+        curr_time = now
+        price = str(symbol.get_price()) + " " + str(symbol.get_quote().get('change')) + " " + str(symbol.get_quote().get('changePercent'))
+
+        return '''<form method="POST">
+              Enter a symbol <input type="text" name="symbol"><br>
+              <input type="submit" value="Submit"><br>
+              </form>
+              <h3>Output: <br/> {} <br/> {} <br/> {} </h3>'''.format(name, curr_time, price)
+
+    return '''<form method="POST">
+                          Enter a symbol <input type="text" name="symbol"><br>
+                          <input type="submit" value="Submit"><br>
+              </form>'''
+
+@app.errorhandler(excep.IEXSymbolError)
+def symbol_error_handle(e):
+    return '''{} Please enter a valid ticker symbol!'''.format(e)
+
+@app.errorhandler(excep.IEXEndpointError)
+def endpoint_error_handle(e):
+    return '''{}'''.format(e)
+
+@app.errorhandler(excep.IEXFieldError)
+def field_error_handle(e):
+    return '''{}'''.format(e)
+
+@app.errorhandler(excep.IEXQueryError)
+def query_error_handle(e):
+    return '''{}'''.format(e)
+
+@app.errorhandler(AttributeError)
+def attribute_error_handle(e):
+    return '''{}'''.format(e)
+
+@app.errorhandler(ValueError)
+def value_error_handle(e):
+    return '''{}'''.format(e)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return '''{}'''. format(e)
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return '''{}'''. format(e)
+
+if __name__ == '__main__':
+    app.run(debug=True)
